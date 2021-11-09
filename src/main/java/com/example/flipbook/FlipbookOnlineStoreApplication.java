d2a69b0d16eda49d586ee8e00e5fe1f8e9207680
jsadej <joanna.sadej@gmail.com>
2021-11-09 09:18:56 +0100

    create

---
+++ src/main/java/com/example/flipbook/FlipbookOnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.flipbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipbookOnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlipbookOnlineStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/flipbook/FlipbookOnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flipbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlipbookOnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
