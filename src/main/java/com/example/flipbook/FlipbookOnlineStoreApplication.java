5210d0811452ead4c71fcc6af42817a4111920d0
jsadej <joanna.sadej@gmail.com>
2022-02-16 14:23:43 +0100

    Dodanie pierwszego beana springowego

--- src/main/java/com/example/flipbook/FlipbookOnlineStoreApplication.java
+++ src/main/java/com/example/flipbook/FlipbookOnlineStoreApplication.java
@@ -1,13 +1,27 @@
 package com.example.flipbook;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class FlipbookOnlineStoreApplication {
+public class FlipbookOnlineStoreApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlipbookOnlineStoreApplication.class, args);
 	}
+	private final  CatalogServices catalogServices;
 
+	public FlipbookOnlineStoreApplication(CatalogServices catalogServices) {
+		this.catalogServices = catalogServices;
+	}
+
+	@Override
+	public void run(String... args)  {
+		//CatalogServices catalogServices = new CatalogServices();
+		List<Book> books=catalogServices.findByTitle("Pan Tadeusz");
+		books.forEach(System.out::println);
+	}
 }
